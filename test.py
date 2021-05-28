eea3d6bb4b2b00342175124ca7155726dd39d7a7
Clement Fleury <clement.fleury@pm.me>
2021-05-28 18:04:35 +0200

    fix test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
+import csv
 from random import *
 from detector import *
 
@@ -7,10 +8,17 @@ from detector import *
 class DetectorTestCase(unittest.TestCase):
 
     def test_detect_language(self):
+        label = {}
         with open('fixtures/x_test.txt') as x_test_file:
             x_test_lines = x_test_file.readlines()
         with open('fixtures/y_test.txt') as y_test_file:
             y_test_lines = y_test_file.readlines()
+        with open('fixtures/labels.csv') as labels_file:
+            reader = csv.reader(labels_file)
+            headers = next(reader)[1:]
+            for row in reader:
+                labels = row[0].split(';')
+                label[labels[0]] = labels[2]
 
         detector = Detector()
 
@@ -18,7 +26,7 @@ class DetectorTestCase(unittest.TestCase):
             line_number = randint(0, len(x_test_lines))
             text = x_test_lines[line_number]
             detected_language = detector.detect_language(x_test_lines[line_number])[0]['language']
-            expected_language = y_test_lines[line_number].strip()
+            expected_language = label[y_test_lines[line_number].strip()]
 
             print(text, detected_language, expected_language)
 
