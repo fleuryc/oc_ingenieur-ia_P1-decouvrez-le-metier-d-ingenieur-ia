e47b595d1caec36884ec0ffdc55cb323bb1815ad
Clement Fleury <clement.fleury@pm.me>
2021-06-01 12:16:50 +0200

    reork

--- detector.py
+++ detector.py
@@ -11,10 +11,21 @@ SUBSCRIPTION_KEY = env.str(
     "TRANSLATOR_TEXT_SUBSCRIPTION_KEY", validate=[Length(equal=32)]
 )
 ENDPOINT = env.str("TRANSLATOR_TEXT_ENDPOINT", validate=[URL()])
-LOCATION = env.str("TRANSLATOR_TEXT_LOCATION", validate=[OneOf(["francecentral"])])
+LOCATION = env.str("TRANSLATOR_TEXT_LOCATION",
+                   validate=[OneOf(["francecentral"])])
 
 
 def detect_language(text: str) -> str:
+    """
+        Detect the language of the input text by calling MS Azure 's Translator
+        Text AI service.
+
+    Args:
+        text (str): Text that we want to detect the language of.
+
+    Returns:
+        str: Detected language in IETF format.
+    """
     headers = {
         "Ocp-Apim-Subscription-Key": SUBSCRIPTION_KEY,
         "Ocp-Apim-Subscription-Region": LOCATION,
